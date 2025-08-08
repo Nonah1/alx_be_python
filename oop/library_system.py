ff6266a8b6ce893670b6f349ab6ee4fc39b3a2e3
Natasha Wanjiku <natashakathambi47@gmail.com>
2025-08-09 00:19:50 +0300

    debugged

--- oop/library_system.py
+++ oop/library_system.py
@@ -10,7 +10,8 @@ class EBook(Book):
 	def __init__(self,title, author, file_size):
 		super().__init__(title, author)
 		self.file_size = file_size
-	def get_description(self):
+
+	def __str__(self):
 		return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
 class PrintBook(Book):
@@ -18,7 +19,7 @@ class PrintBook(Book):
 		super().__init__(title, author)
 		self.page_count = page_count
 	
-	def get_description(self):
+	def __str__(self):
 		return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
 class Library:
