3bc1531a11fe9b1f0b8bbae1800d9a09bedd1cb5
Natasha Wanjiku <natashakathambi47@gmail.com>
2025-08-09 00:26:48 +0300

    OOP"

--- oop/library_system.py
+++ oop/library_system.py
@@ -2,7 +2,7 @@ class Book:
 	def __init__(self, title, author):
 		self.title = title
 		self.author = author
-	def get_description(self):
+	def __str__(self):
 		return f"Book: {self.title} by {self.author}"
 
 
