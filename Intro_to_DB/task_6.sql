6e4a2a410d6ca63b07a0ebd7004d2e4c4b292860
Natasha Wanjiku <natashakathambi47@gmail.com>
2025-08-17 22:05:35 +0300

    Database works

---
+++ Intro_to_DB/task_6.sql
@@ -0,0 +1,5 @@
+INSERT INTO customers (customer_id, first_name, last_name, email)
+VALUES 
+(2, 'Blessing', 'Malik', 'bmalik@sandtech.com'),
+(3, 'Obed', 'Ehoneah', 'eobed@sandtech.com'),
+(4, 'Nehemial', 'Kamolu', 'nkamolu@sandtech.com'); 
